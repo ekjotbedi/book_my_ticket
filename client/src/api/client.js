c7f0e3e6ae2a5d2a37cb1f6a4da98feebbedf17b
ekjotbedi <ekjotbedi2006@gmail.com>
2026-07-03 13:30:32 -0400

    Create client.js

---
+++ client/src/api/client.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const api = axios.create({ baseURL: "/api" });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export function errorMessage(err) {
+  return (
+    err?.response?.data?.error ||
+    err?.response?.data?.message ||
+    err?.message ||
+    "Something went wrong"
+  );
+}
